46c922b0e7a65ac14ec4721453c5c4a7f2ba3546
Braden Chamberlain <158085947+braden-chamberlain@users.noreply.github.com>
2024-04-30 14:17:54 -0600

    Update data.py
    Completed ticket

--- app/data.py
+++ app/data.py
@@ -1,5 +1,4 @@
 from os import getenv
-
 from certifi import where
 from dotenv import load_dotenv
 from MonsterLab import Monster
@@ -8,18 +7,32 @@ from pymongo import MongoClient
 
 
 class Database:
+    load_dotenv()
+    database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Database"]
+
+    def __init__(self, collection: str):
+        """initializes the database with MongoDB"""
+        self.collection = self.database[collection]
 
     def seed(self, amount):
-        pass
+        """Generates dictionaries of monsters and adds them to the database"""
+        documents = [Monster().to_dict() for _ in range(amount)]
+        self.collection.insert_many(documents)
 
     def reset(self):
-        pass
+        """Deletes all documents from collection"""
+        self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        """counts the number of documents in the collection"""
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        """converts collection into a pandas dataframe"""
+        return DataFrame(self.collection.find({}))
 
     def html_table(self) -> str:
-        pass
+        if self.count() == 0:
+            return 'None'
+        else:
+            return self.dataframe().to_html()
