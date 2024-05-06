79643585c8dff4becf888e127da355a90524aa66
Braden Chamberlain <158085947+braden-chamberlain@users.noreply.github.com>
2024-05-06 15:14:52 -0600

    Update machine.py

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,37 @@
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+from joblib import dump, load
+from datetime import datetime
+
+
 class Machine:
 
-    def __init__(self, df):
-        pass
+    def __init__(self, df: DataFrame = None, model=None):
+        self.name = "Random Forest Classifier"
+        if df is not None:
+            target = df["Rarity"]
+            features = df.drop(columns=["Rarity"])
+            self.model = RandomForestClassifier()
+            self.model.fit(features, target)
+        elif model is not None:
+            self.model = model
 
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, pred_basis: DataFrame):
+        prediction = self.model.predict(pred_basis)[0]
+        confidence = max(self.model.predict_proba(pred_basis)[0])
+        return prediction, confidence
 
     def save(self, filepath):
-        pass
+        dump(self.model, filepath)
 
     @staticmethod
     def open(filepath):
-        pass
+        model = load(filepath)
+        return Machine(model=model)
 
-    def info(self):
-        pass
+    def info(self, pred_basis: DataFrame):
+        prediction = self.__call__(pred_basis)
+        confidence = max(self.model.predict_proba(pred_basis)[0])
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return f"Timestamp: {timestamp}, Prediction: {prediction}, Confidence: {confidence}"
+
